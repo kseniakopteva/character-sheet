d7f4bc9ac711b2afd30209e4a93f0d4689a11e5c
kseniakopteva <ksenija.kopteva@gmail.com>
2026-02-11 12:59:05 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+const App = () => {
+	return <RouterProvider router={router} />;
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+			},
+			"/public": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+			},
+		},
+	},
+	plugins: [TanStackRouterVite(), react(), tailwindcss()],
+});
